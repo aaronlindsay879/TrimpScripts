49914c29aae7105000a749adf6d4831d97cc9771
slivermasterz <lokmanslivermaster@gmail.com>
2018-05-14 16:35:21 -0700

    Minor changes
    - Changed Autoallocate
    - Added Gene reset
    - optimized anti reflect

--- Trimps Scripts.js
+++ Trimps Scripts.js
@@ -1,5 +1,6 @@
 var perked = false;
 var prestiged = false;
+var resetGenes = false;
 enableDebug = false;
 autoTrimpSettings["VoidMaps"].value = 510;
 autoTrimpSettings["ExitSpireCell"].value = 40;
@@ -8,20 +9,21 @@ autoTrimpSettings["AutoStartDaily"].enabled = false;
 setInterval(
     function(){
         //Windstacking stance dancing
-        if(game.global.world>=80) {
-            if( getEmpowerment() !== "Wind" || game.global.mapsActive || game.empowerments.Wind.currentDebuffPower===200) {
-                if (!(game.global.mapsActive && game.global.mapsOwnedArray[getMapIndex(game.global.currentMapId)].bonus === "lmc"))
-                {
+        if(game.global.world>=70) {
+            if (game.global.dailyChallenge.hasOwnProperty("mirrored") && game.global.world < 230) {
+                setFormation(1);
+            }
+            else if (getEmpowerment() !== "Wind" || game.global.mapsActive || game.empowerments.Wind.currentDebuffPower === 200) {
+                if (!(game.global.mapsActive && game.global.mapsOwnedArray[getMapIndex(game.global.currentMapId)].bonus === "lmc")) {
                     setFormation(2);
                 }
-                if (getEmpowerment() !== "Wind")
-                {
+                if (getEmpowerment() !== "Wind") {
                     MODULES["maps"].enoughDamageCutoff = 4;
                 }
             }
             else if (game.global.challengeActive === "Daily" && !game.global.spireActive) {
                 setFormation(4);
-                if (game.global.gridArray[game.global.lastClearedCell+1].corrupted === "corruptBleed" || game.global.gridArray[game.global.lastClearedCell+1].corrupted ==="healthyBleed") {
+                if (game.global.gridArray[game.global.lastClearedCell + 1].corrupted === "corruptBleed" || game.global.gridArray[game.global.lastClearedCell + 1].corrupted === "healthyBleed") {
                     setFormation(2);
                 }
                 MODULES["maps"].enoughDamageCutoff = 160;
@@ -32,7 +34,7 @@ setInterval(
             fightManual();
             buyArmors();
         }
-        if (game.global.antiStacks !== 45 && game.global.lastBreedTime >= 45000 && !game.global.SpireActive) {
+        if (game.global.antiStacks !== 45 && game.global.lastBreedTime >= 45000 && !game.global.spireActive) {
             forceAbandonTrimps();
         }
         if ((needPrestige || !enoughDamage) && game.global.world>=200 && (getEmpowerment() === "Ice" || (getEmpowerment() === "Wind" && game.global.lastBreedTime >= 45000)) && !game.global.mapsActive && game.global.mapBonus !== 10 && game.global.world!==game.options.menu.mapAtZone.setZone) {
@@ -40,6 +42,15 @@ setInterval(
         }
         if (game.global.world === autoTrimpSettings["VoidMaps"].value && game.global.lastClearedCell >= 80 && getPageSetting('AutoMaps') === 0){
             toggleAutoMaps();
+        }
+        if (game.global.world === 495 && !resetGenes)
+        {
+            game.global.genPaused = true;
+            numTab('6');
+            game.global.firing = true;
+            buyJob('Geneticist');
+            game.global.firing = false;
+            resetGenes = true;
         }
         //Raiding logic
         if (game.global.world === game.options.menu.mapAtZone.setZone && game.options.menu.mapAtZone.enabled === 1) {
@@ -69,19 +80,20 @@ setInterval(
         }
 
         //Resetting values
-        if (game.global.world <= 10){
+        if (game.global.world <= 10 && game.global.dailyChallenge.hasOwnProperty("mirrored")){
             autoTrimpSettings["BuyWeapons"].enabled = false;
         }
         else if (game.global.world===230){
             perked = false;
             prestiged = false;
+            resetGenes = false;
             game.options.menu.mapAtZone.setZone = 495;
             game.options.menu.mapAtZone.enabled = 1;
             autoTrimpSettings["BuyWeapons"].enabled = true;
             autoTrimpSettings["AutoMaps"].value = 1;
         }
         //AutoAllocate Looting II
-        if (!perked){
+        if (!perked && game.global.world !== 230){
             viewPortalUpgrades();
             game.global.lastCustomAmt = 100000;
             numTab(5, true);
@@ -92,7 +104,7 @@ setInterval(
             }
             else{
                 perked = true;
-                setTimeout(cancelPortal(), 1000);
+                cancelPortal();
                 message("Done buying Looting II","Notices");
             }
         }
